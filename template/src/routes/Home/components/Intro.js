932e8c2439db3d8a041954ed4eb0e56b14682931
Lee <likun7981@gmail.com>
2017-10-27 14:42:49 +0800

    update dependencies

--- template/src/routes/Home/components/Intro.js
+++ template/src/routes/Home/components/Intro.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Intro.less';
-import {Alert} from 'antd';
+import { Alert } from 'antd';
 
 export default () => (
-  <Alert message={['To get started, edit ', <code>src/App.js {process.env.REACT_APP_HOST}</code> , ' and save to reload.']} />
+  <Alert
+    message={
+      <div>
+        To get started, edit 
+        <code> src/App.js {process.env.REACT_APP_HOST}</code> and save to
+        reload.
+      </div>
+    }
+  />
 );
